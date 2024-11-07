f3ff1545e1c2b2b708e1d2cecfe83232c509ef60
Milena <milena.caroline@alunoescolassociais.org.br>
2024-11-07 12:14:16 -0300

    redefine

--- cont.js
+++ cont.js
@@ -1,27 +1,43 @@
-document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    // obter valores de entrada informados pelo usuário
-    const valor = parseFloat(document.getElementById("amount").value);
-    const daMoeda = document.getElementById("daMoeda").value;
-    const paraMoeda = document.getElementById("paraMoeda").value;
-   
-    // definir taxas de cambio fixas
-    const exchangeRates = {
-      USD: { BRL: 5.7, EUR: 0.93 },
-      BRL: { USD: 0.18, EUR: 0.16 },
-      EUR: { USD: 1.08, EUR: 6.16 },
-    };
-    // Conversão simples de moedas
-    let valorConvertido;
-    if (daMoeda === paraMoeda) {
-      valorConvertido = valor;
-    } else {
-      valorConvertido = valor * exchangeRates[daMoeda][paraMoeda];
+const apiKey = `6c2e94c0c364f63d85c4621b`
+const apiURL =  `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD `
+
+async function getExchangeRates(deMoeda, paraMoeda){
+    try {
+        const response = await fetch(`${apiUrl}${daMoeda}`);
+        const data = response.json();
+ 
+        if (data === "success") {
+            return data.conversion_rates[paraMoeda];
+        } else {
+            throw new Error("Erro ao buscar taxa de câmbio");
+        }
+    } catch (error) {
+        console.error("Error", error);
+        return null;
     }
-   
-    const Conversão = document.getElementById("conversão");
-    Conversão.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda} `;
-  });
+}
 
 // ###############################################################################
 
+document.getElementById('form').addEventListener('submit', async function(event){ 
+    event.preventDefault();
+
+    // Obter valores de entrada 
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+    const exchangeRate = await getExchangeRate(daMoeda, paraMoeda);
+
+    if(exchangeRate){
+        const covertedValue = valor * exchangeRates;
+
+
+        // console.log(covertedValue);
+
+        const conversao = document.getElementById('conversão');
+        conversao.textContent = `Resultado: ${convertedValue.toFixed(2)}$ {paraMoeda}`;
+    } else{ 
+        alert('Erro ao buscar a cotação. Tente novamente');
+    }
+})
